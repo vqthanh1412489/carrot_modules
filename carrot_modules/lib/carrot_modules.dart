d8badba9184a6143a4aab7e61e3fcdc9710ee1bc
Vu Thanh <vqthanh1412489@gmail.com>
2023-07-12 13:58:40 +0700

    add carrot_shimmer

--- carrot_modules/lib/carrot_modules.dart
+++ carrot_modules/lib/carrot_modules.dart
@@ -3,3 +3,4 @@ library carrot_percent_indicator;
 
 export 'src/carrot_dotted_border/carrot_dotted_border.dart';
 export 'src/carrot_percent_indicator/carrot_percent_indicator.dart';
+export 'src/carrot_shimmer/carrot_shimmer.dart';
