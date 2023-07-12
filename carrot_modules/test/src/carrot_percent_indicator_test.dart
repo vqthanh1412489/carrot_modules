4a6970b3aef221fc6170501d336315b02a4ac907
Vu Thanh <vqthanh1412489@gmail.com>
2023-07-12 10:36:21 +0700

    change name of modules

--- carrot_modules/test/src/carrot_percent_indicator_test.dart
+++ carrot_modules/test/src/carrot_percent_indicator_test.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:carrot_percent_indicator/carrot_percent_indicator.dart';
+import 'package:carrot_modules/carrot_modules.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('CarrotPercentIndicator', () {
     test('can be instantiated', () {
-      expect(CarrotPercentIndicator(), isNotNull);
+      expect(CarrotPercentIndicator, isNotNull);
     });
   });
 }
